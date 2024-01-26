b33d24291147d4e407e1735438815767a4e88908
Irbanology <khanirban750@gmail.com>
2024-01-26 14:32:45 +0500

    First Project

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-// var inp = document.getElementById('inputnode').value;
+const inpNode = document.getElementById('inputnode')
 
-// function dbao(e) {
-//     console.log(e);
-//     document.getElementById('inputnode').value += e;
-// }
-// function ac() {
-//     document.getElementById('inputnode').value = "";
-
-// } 
-// function del() {
-//     var currentValue = document.getElementById('inputnode').value;
-//     document.getElementById('inputnode').value = currentValue.slice(0, -1);
-// }
-
-// function equal(){
-//     var nn = document.getElementById('inputnode').value;
-//     var mn = eval(nn.value);
-//     document.getElementById('inputnode').value = mn
-
-// }
-
-
-
-function dbao(e) {
+function Btns(e) {
     console.log(e);
     document.getElementById('inputnode').value += e;
 }
@@ -41,3 +19,4 @@ function equal() {
     var mn = eval(nn);
     document.getElementById('inputnode').value = mn;
 }
+
